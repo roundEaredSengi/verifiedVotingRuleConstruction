1e52858864a469acdeb5989ec327f683a74e34a4
Alicia Appelhagen <uoxyc@student.kit.edu>
2025-05-12 05:19:45 +0200

    Add proof that symmetr. VP + anon. voting rule + well-formedness assumptions imply identical VP for all voters

--- theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Power.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Power.thy
@@ -2,7 +2,7 @@ section \<open>Voting Power\<close>
 
 theory Voting_Power
   imports Electoral_Module
-          Distance                     
+          Distance    
           "HOL-Probability.Probability_Measure"
 
 begin
@@ -14,6 +14,25 @@ type_synonym ('a, 'v, 'r) Swing_Weight =
 
 type_synonym ('a, 'v, 'r) Voting_Power =
   "('a, 'v, 'r) Electoral_Module \<Rightarrow> ('a, 'v) Election set \<Rightarrow> 'v \<Rightarrow> ereal"
+
+text \<open>Symmetry of voting power measures\<close>
+
+type_synonym ('a, 'v, 'r) Voting_Power_Domain =
+  "('a, 'v, 'r) Electoral_Module \<times> ('a, 'v) Election set \<times> 'v"
+
+fun rename_rule :: "('v \<Rightarrow> 'v) \<Rightarrow> ('a, 'v, 'r) Electoral_Module \<Rightarrow> ('a, 'v, 'r) Electoral_Module" where
+  "rename_rule \<pi> f = (\<lambda> V A p. (fun\<^sub>\<E> f) ((the_inv (rename \<pi>)) (A, V, p)))"
+
+fun rename_pow :: 
+  "('v \<Rightarrow> 'v) \<Rightarrow> (('a, 'v, 'r) Voting_Power_Domain \<Rightarrow> ('a, 'v, 'r) Voting_Power_Domain)" where
+  "rename_pow \<pi> (f, E, v) = (rename_rule \<pi> f, (rename \<pi>) ` E, \<pi> v)"
+
+fun uncurry3 :: "('w \<Rightarrow> 'x \<Rightarrow> 'y \<Rightarrow> 'z) \<Rightarrow> (('w \<times> 'x \<times> 'y) \<Rightarrow> 'z)" where
+  "uncurry3 f = (\<lambda>(w,x,y). f w x y)"
+
+fun is_sym_pow :: "('a, 'v, 'r) Voting_Power_Domain set \<Rightarrow> ('a, 'v, 'r) Voting_Power \<Rightarrow> bool" where
+  "is_sym_pow X \<delta> = (is_symmetry (uncurry3 \<delta>) (Invariance 
+    (action_induced_rel (Bij (UNIV::('v set))) X (\<lambda> \<pi>. rename_pow \<pi>))))"
 
 text \<open>Voting power as weighted sum over swing elections.\<close>
 fun voting_power :: 
@@ -68,19 +87,114 @@ locale idd_prob_space = prob_space +
   prob_space "(\<Omega>::'a set, A, \<mu>)" *)
 
 theorem banzhaf_is_probability:
-  "True"
-  sorry
+  "False"
+  oops
+
+fun swap_voters :: "'v \<Rightarrow> 'v \<Rightarrow> ('a, 'v) Election \<Rightarrow> ('a, 'v) Election" where
+  "swap_voters v w e = 
+    (let \<pi> = (\<lambda>x::'v. (if x = w then v else (if x = v then w else x))) in
+      rename \<pi> e)"
+
+lemma rename_inv_commute:
+  fixes
+    \<pi> :: "'v \<Rightarrow> 'v"
+  assumes "bij \<pi>"
+  shows
+    "the_inv (rename \<pi>) = rename (the_inv \<pi>)"
+proof -
+  have "(rename (the_inv \<pi>)) \<circ> (rename \<pi>) = id"
+    unfolding comp_def
+    using rename_inv ext assms id_def inj_def prod_cases3 rename_inj
+    by (metis (no_types, lifting))
+  thus "the_inv (rename \<pi>) = rename (the_inv \<pi>)"
+    using  ext assms bij_betw_the_inv_into comp_apply id_apply rename_inj the_inv_f_f
+    by (metis (no_types, lifting))
+qed
 
 lemma (in idd_prob_space) invar_func_imp_invar_prob:
-   fixes
-    f :: "'a \<Rightarrow> 'b" and
-    S :: "'a set" and
-    T :: "'c set" and
-    \<phi> :: "('c, 'a) binary_fun"
-  assumes "is_symmetry f (Invariance (action_induced_rel T S \<phi>))"
-  shows "is_symmetry (emeasure M) 
-          (Invariance (action_induced_rel UNIV UNIV (set_action \<phi>)))"
-  (* TODO: Replace the UNIV's by sensible sets *)
-  sorry
+  fixes
+    \<delta> :: "('a, 'v, 'r) Voting_Power" and
+    f :: "('a, 'v, 'r) Electoral_Module" and
+    E :: "('a, 'v) Election set" and
+    X :: "('a, 'v, 'r) Voting_Power_Domain set" and
+    v :: 'v and
+    w :: 'v 
+  assumes 
+    sym_pow: "is_sym_pow X \<delta>" and
+    anon_rule: "anonymity_in E f" and
+    well_formed_counting_domain: "swap_voters v w ` E  = E" and
+    domain_constrained_delta: 
+      "\<forall> f g Y x. (\<forall> e \<in> Y. 
+        f (voters_\<E> e) (alternatives_\<E> e) (profile_\<E> e) = 
+        g (voters_\<E> e) (alternatives_\<E> e) (profile_\<E> e))
+        \<longrightarrow> \<delta> f Y x = \<delta> g Y x" and
+    valid_v: "(f, E, v) \<in> X"
+  shows "\<delta> f E v = \<delta> f E w"
+proof -
+  (* Generally, the symmetric power of v under f, E equals that of pi(v) under f_pi, pi(E). *)
+  let ?\<pi> = "(\<lambda> x :: 'v. (if x = w then v else (if x = v then w else x)))"
+  have bij0: "bij ?\<pi> \<and> bij (the_inv ?\<pi>)"
+    by (simp add: bij_betw_the_inv_into involuntory_imp_bij)
+  hence bij: "?\<pi> \<in> Bij UNIV \<and> (the_inv ?\<pi>) \<in> Bij UNIV"
+    unfolding Bij_def extensional_def
+    by simp
+  moreover have 
+    "((rename_rule ?\<pi> f), ((rename ?\<pi>) ` E), (?\<pi> v)) = rename_pow ?\<pi> (f, E, v)"
+    by simp
+  ultimately have 
+    "\<exists> x \<in> Bij UNIV. 
+      rename_pow x (f, E, v) = ((rename_rule ?\<pi> f), ((rename ?\<pi>) ` E), (?\<pi> v))"
+    using valid_v
+    by metis
+  hence
+    "((f, E, v), ((rename_rule ?\<pi> f), ((rename ?\<pi>) ` E), (?\<pi> v))) \<in> 
+      action_induced_rel (Bij (UNIV::('v set))) X (\<lambda> \<pi>. rename_pow \<pi>)"
+    using valid_v
+    by simp
+  hence eq0: "\<delta> f E v = \<delta> (rename_rule ?\<pi> f) ((rename ?\<pi>) ` E) (?\<pi> v)"
+    using sym_pow
+    by simp
+  (* pi(v) = w, E_pi = E and f_pi = f (constrained to E) yields the result 
+    given that delta does not care about values of f outside of E. *)
+  have 
+    "\<forall> e \<in> E. extensional_continuation (the_inv (rename ?\<pi>)) E e = 
+                (the_inv (rename ?\<pi>)) e"
+    by simp
+  hence 
+    "\<forall> e \<in> E. \<exists> x \<in> Bij UNIV. 
+      extensional_continuation (the_inv (rename ?\<pi>)) E e = 
+      (the_inv (rename ?\<pi>)) e"
+    using bij id_Bij
+    unfolding \<phi>_anon.simps
+    by blast
+  hence "\<forall> e \<in> E. \<exists> x \<in> Bij UNIV. \<phi>_anon E x e = (the_inv (rename ?\<pi>)) e"
+    unfolding \<phi>_anon.simps
+    using rename_inv_commute
+    by (metis (no_types, lifting) bij0 bij)
+  hence "\<forall> e \<in> E. (e, (the_inv (rename ?\<pi>)) e) \<in> anonymity\<^sub>\<R> E"
+    using bij
+    unfolding anonymity\<^sub>\<R>.simps action_induced_rel.simps bijection\<^sub>\<V>\<^sub>\<G>_def BijGroup_def
+    by simp
+  hence
+    "\<forall> e \<in> E. rename_rule ?\<pi> f (voters_\<E> e) (alternatives_\<E> e) (profile_\<E> e) = 
+                f (voters_\<E> e) (alternatives_\<E> e) (profile_\<E> e)"
+    using anon_rule alternatives_\<E>.elims prod.collapse profile_\<E>.elims voters_\<E>.elims
+    unfolding anonymity_in.simps is_symmetry.simps fun\<^sub>\<E>.simps rename_rule.simps
+    by (metis (lifting))
+  hence "\<delta> f E w = \<delta> (rename_rule ?\<pi> f) E w"
+    using domain_constrained_delta
+    by presburger
+  moreover have "w = ?\<pi> v"
+    by simp
+  moreover have "E = (rename ?\<pi>) ` E"
+    using well_formed_counting_domain
+    by simp
+  ultimately have "\<delta> f E w = \<delta> (rename_rule ?\<pi> f) ((rename ?\<pi>) ` E) (?\<pi> v)"
+    by simp
+  thus "\<delta> f E v = \<delta> f E w"
+    using eq0
+    by simp
+    (* by argo *)
+qed
 
 end
