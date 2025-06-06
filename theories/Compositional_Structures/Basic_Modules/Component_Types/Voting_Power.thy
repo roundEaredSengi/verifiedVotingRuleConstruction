559c725fe5d635b300b58ef9f2906e5e5f0a869d
Alicia Appelhagen <uoxyc@student.kit.edu>
2025-06-06 18:18:48 +0200

    Change lemma name

--- theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Power.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Power.thy
@@ -35,10 +35,10 @@ fun is_sym_pow :: "('a, 'v, 'r) Voting_Power_Domain set \<Rightarrow> ('a, 'v, '
     (action_induced_rel (Bij (UNIV::('v set))) X (\<lambda> \<pi>. rename_pow \<pi>))))"
 
 text \<open>Voting power as weighted sum over swing elections.\<close>
-fun voting_power :: 
+fun weighted_voting_power :: 
   "('a, 'v, 'r) Electoral_Module \<Rightarrow> ('a, 'v) Election set \<Rightarrow>
   ('a, 'v, 'r) Swing_Weight \<Rightarrow> 'v \<Rightarrow> ereal" where
-  "voting_power f E weight v = (\<Sum> e \<in> E. weight f e v)"
+  "weighted_voting_power f E weight v = (\<Sum> e \<in> E. weight f e v)"
 
 fun coincide_except :: "('a, 'v) Election \<Rightarrow> ('a, 'v) Election \<Rightarrow> 'v \<Rightarrow> bool" where
   "coincide_except e1 e2 v = 
@@ -61,34 +61,16 @@ text \<open>
   Raw power defines the weights of swing elections as the 
   total result distance achievable via swing voting.\<close>
 fun raw_power :: "'r Distance \<Rightarrow> ('a, 'v, 'r) Voting_Power" where
-  "raw_power d f E v = voting_power f E (raw_weight d E) v"
+  "raw_power d f E v = weighted_voting_power f E (raw_weight d E) v"
 
 subsection \<open>Specific Power Indices\<close>
 
-text \<open>
-  A generalized Banzhaf can be taken to count the profiles where
-  a voter has at least one swing vote, but also the swing votes.
-  We count the profiles where v has at least one swing vote,
-  aiming at a probabilistic interpretation as 
-  "probability of having a swing vote".
-\<close>
-fun banzhaf_prob :: "('a, 'v, 'r) Voting_Power" where
-  "banzhaf_prob f E v = 0" (* TODO!1!!111 *)
-
-(* TODO this counts each swing vote separately, so the normalization doesn't make sense... *)
 fun banzhaf\<^sub>r\<^sub>a\<^sub>w :: "('a, 'v, 'r) Voting_Power" where
   "banzhaf\<^sub>r\<^sub>a\<^sub>w f E v = (1/(ereal (card E))) * (raw_power discrete_dist f E v)"
 
 text \<open>Identically distributed probability space.\<close>
 locale idd_prob_space = prob_space +                   
   assumes "\<forall> S \<subseteq> space M. emeasure M S = (card S)/(card (space M))"
-
-(* global_interpretation banzhaf_probability_space:
-  prob_space "(\<Omega>::'a set, A, \<mu>)" *)
-
-theorem banzhaf_is_probability:
-  "False"
-  oops
 
 fun swap_voters :: "'v \<Rightarrow> 'v \<Rightarrow> ('a, 'v) Election \<Rightarrow> ('a, 'v) Election" where
   "swap_voters v w e = 
@@ -111,7 +93,7 @@ proof -
     by (metis (no_types, lifting))
 qed
 
-lemma (in idd_prob_space) invar_func_imp_invar_prob:
+lemma anon_rule_imp_symmetry_pow_const:
   fixes
     \<delta> :: "('a, 'v, 'r) Voting_Power" and
     f :: "('a, 'v, 'r) Electoral_Module" and
@@ -194,7 +176,6 @@ proof -
   thus "\<delta> f E v = \<delta> f E w"
     using eq0
     by simp
-    (* by argo *)
 qed
 
 end
