e774a9d9e5c29282d0383e3b39d240bed8e2972f
Alicia Appelhagen <uoxyc@student.kit.edu>
2025-10-12 20:27:56 +0200

    Rework voting model isomorphism locale

--- theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Models.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Voting_Models.thy
@@ -37,10 +37,15 @@ locale voting_model =
     \<V> :: "'v set" and
     \<B> :: "'b set" and
     \<O> :: "'o set" and
-    \<M> :: "'x" and
-    f :: "('v \<Rightarrow> 'b) \<Rightarrow> 'o"
+    \<M> :: "'x" and 
+      (* 
+        \<M> is the mathematical structure representing the decision process.
+        It may contain details that are ultimately irrelevant to the tallying method.
+      *)
+    f :: "('v \<Rightarrow> 'b) \<Rightarrow> 'o" 
+      (* Intuitively, f describes the core of \<M> as a decision process. *)
   assumes
-    valid_outcomes: "\<forall>p::'v \<Rightarrow> 'b. p ` \<V> \<subseteq> \<B> \<longrightarrow> f p \<in> \<O>"
+    valid_outcomes: "\<forall> p::'v \<Rightarrow> 'b. p ` \<V> \<subseteq> \<B> \<longrightarrow> f p \<in> \<O>"
 
 text \<open>
   A voting rule models the decision procedure of a voting system as the tallying method itself.
@@ -81,6 +86,71 @@ locale simple_voting_game = (* TODO model using 0, 1 instead of Booleans *)
 sublocale simple_voting_game \<subseteq> 
   voting_model \<V> "UNIV::(bool set)" "UNIV::(bool set)" \<G> "\<lambda>p. value_fun \<G> (preimg p \<V> True)" 
 proof (unfold_locales) qed
+
+subsection \<open>Comparison of Voting Models\<close>
+
+text \<open>
+  For two voting models with different descriptions \<M>, \<M>' of the decision process
+  to be isomorphic, the voters, ballots, outcomes and tallying method necessarily need to be equal.
+  Further requirements about isomorphism of \<M>, \<M>' can be added.
+\<close>
+(* 
+  TODO too strict, only require bijections instead of identity between all components but \<M>, \<M>'?  
+*)
+locale model_isomorphism = 
+  m1: voting_model \<V> \<B> \<O> \<M> f + m2: voting_model \<V> \<B> \<O> \<M>' f
+  for 
+    \<V> :: "'v set" and
+    \<B> :: "'b set" and
+    \<O> :: "'o set" and
+    \<M> :: "'x" and \<M>' :: "'y" and
+    f :: "('v \<Rightarrow> 'b) \<Rightarrow> 'o" +
+  fixes 
+    isomorphism :: "'x \<Rightarrow> 'y \<Rightarrow> bool"
+  assumes
+    "isomorphism \<M> \<M>'"
+
+subsection \<open>Exemplary Instantiations\<close>
+
+text \<open> 
+  Any voting model is isomorphic to itself if decision processes are required to be identical.
+\<close>
+lemma self_isomorphism:
+  fixes
+    \<V> :: "'v set" and
+    \<B> :: "'b set" and
+    \<O> :: "'o set" and
+    \<M> :: "'x" and
+    f :: "('v \<Rightarrow> 'b) \<Rightarrow> 'o"
+  assumes
+    "voting_model \<V> \<B> \<O> f"
+  shows
+    "model_isomorphism \<V> \<B> \<O> \<M> \<M> f (\<lambda>x y. x = y)"
+proof (unfold_locales, simp_all)
+  show "\<forall>p. p ` \<V> \<subseteq> \<B> \<longrightarrow> f p \<in> \<O>"
+    using assms
+    unfolding voting_model_def
+    by blast
+qed
+
+lemma rule_svg_isomorphism:
+  fixes
+    \<V> :: "'v set" and
+    \<F> :: "('v \<Rightarrow> bool) \<Rightarrow> bool" and
+    \<G> :: "'v Simple_Voting_Game" and
+    f :: "('v \<Rightarrow> bool) \<Rightarrow> bool" and
+    isomorphism :: "(('v \<Rightarrow> bool) \<Rightarrow> bool) \<Rightarrow> 'v Simple_Voting_Game \<Rightarrow> bool"
+  assumes
+    "voting_rule \<V> (UNIV::bool set) (UNIV::bool set) \<F>" and
+    "simple_voting_game \<V> \<G>" and
+    "model_isomorphism \<V> (UNIV::bool set) (UNIV::bool set) \<F> \<G> f isomorphism"
+  shows
+    "f = \<F>" and "f = (\<lambda>p. value_fun \<G> (preimg p \<V> True))"
+    (* 
+      TODO won't be able to show that? 
+      There should be counterexamples in the current implementation - fix that!
+    *)
+  sorry
 
 interpretation majority_svg_5_voters:
   simple_voting_game "{1,2,3,4,5}" "({1,2,3,4,5}, \<lambda>S. card S \<ge> 3)"
